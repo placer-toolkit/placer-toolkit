3d52700b3e3e7fdc60ee5381c8bcadcd290043be
= <150704902+randomguy-2650@users.noreply.github.com>
2025-12-06 12:16:00 +0100

    Add partial localisation

---
+++ docs/src/i18n/ui.ts
@@ -0,0 +1,145 @@
+export const languages = {
+    en: "English",
+    de: "Deutsch",
+};
+
+export const defaultLanguage = "en";
+
+export const showDefaultLanguage = false;
+
+export const ui = {
+    en: {
+        // UI and tools
+        "ui.toc.onThisPage": "On this page",
+        "ui.toc.overview": "Overview",
+        "ui.search.button": "Search",
+        "ui.search.searchNotAvailableInDevBuilds":
+            "Search is only available in production builds.<br />Try building and previewing the site to test it out locally.",
+        "ui.keyboard.ctrl": "Ctrl",
+        "ui.content.contentNotAvailableInYourLanguage":
+            "This content is not available in your language yet.",
+
+        // Landing page and marketing
+        "getStartedBanner.readyToStart": "Ready to build with Placer Toolkit?",
+        "landingCTA.marketingLine":
+            "Native web components for<br />modern interfaces",
+        "landingCTA.marketingLine.descriptive":
+            "Placer Toolkit is an open‐source and framework‐agnostic web component library to build modern interfaces.",
+        "landingCTA.tagline.flexible": "Flexible.",
+        "landingCTA.tagline.accessible": "Accessible.",
+        "landingCTA.tagline.webComponents": "Web Components.",
+
+        // Feature tiles
+        "featureTiles.entirelyNative": "Entirely native",
+        "featureTiles.entirelyNativeMessage":
+            "Built with web standards, Placer Toolkit works in any framework—or none at all. <em>No locking in, no compromises.</em>",
+        "featureTiles.fullyAccessible": "Fully accessible",
+        "featureTiles.fullyAccessibleMessage":
+            'Designed with <span aria-label="accessibility">a11y</span>, <span aria-label="internationalisation">i18n</span> (71 locales out-of-the-box) and strong DX in mind. <em>Everyone can use it, everywhere.</em>',
+        "featureTiles.flexible": "Flexible",
+        "featureTiles.flexibleMessage":
+            "Easily restyle, customise and adapt components to match your design. <em>Your website, your rules.</em>",
+
+        // Labels
+        "button.getStarted": "Get started",
+        "button.ourPrivacyPromise": "Our privacy promise",
+
+        // Footer headings
+        "footer.heading.docs": "Docs",
+        "footer.heading.about": "About",
+        "footer.heading.legal": "Legal",
+
+        // Footer links (Docs)
+        "footer.content.docs.installation": "Installation",
+        "footer.content.docs.usage": "Usage",
+        "footer.content.docs.designTokens": "Design tokens",
+        "footer.content.docs.components": "Components",
+        "footer.content.docs.changelog": "Changelog",
+
+        // Footer links (About)
+        "footer.content.about.community": "Community",
+        "footer.content.about.contributing": "Contributing",
+        "footer.content.about.brandAssets": "Brand assets",
+        "footer.content.about.support": "Support",
+
+        // Footer links (Legal)
+        "footer.content.legal.privacy": "Privacy",
+        "footer.content.legal.accessibility": "Accessibility",
+        "footer.content.legal.codeOfConduct": "Code of Conduct",
+        "footer.content.legal.licence": "Licence",
+        "footer.content.legal.impressum": "Impressum",
+
+        // Footer copyright
+        "footer.copyright.left": "Made with",
+        "footer.copyright.centre": "love",
+        "footer.copyright.right":
+            'by Placer and its contributors • <span style="white-space: nowrap">© 2025</span>',
+    },
+    de: {
+        // UI and tools
+        "ui.toc.onThisPage": "Auf dieser Seite",
+        "ui.toc.overview": "Überblick",
+        "ui.search.button": "Suchen",
+        "ui.search.searchNotAvailableInDevBuilds":
+            "Die Suche ist nur in Produktions‐Builds verfügbar.<br />Baue die Website und sieh dir die Vorschau an, um sie lokal zu testen.",
+        "ui.keyboard.ctrl": "Strg",
+        "ui.content.contentNotAvailableInYourLanguage":
+            "Dieser Inhalt ist noch nicht in deiner Sprache verfügbar.",
+
+        // Landing page and marketing
+        "getStartedBanner.readyToStart": "Bereit, mit Placer Toolkit zu bauen?",
+        "landingCTA.marketingLine":
+            "Native Webkomponenten für<br />moderne Benutzeroberflächen",
+        "landingCTA.marketingLine.descriptive":
+            "Placer Toolkit ist eine Open‐Source‐ und framework‐agnostische Webkomponenten‐Bibliothek zum Erstellen moderner Benutzeroberflächen.",
+        "landingCTA.tagline.flexible": "Flexibel.",
+        "landingCTA.tagline.accessible": "Barrierefrei.",
+        "landingCTA.tagline.webComponents": "Webkomponenten.",
+
+        // Feature tiles
+        "featureTiles.entirelyNative": "Vollständig nativ",
+        "featureTiles.entirelyNativeMessage":
+            "Placer Toolkit basiert auf Webstandards und funktioniert in jedem Framework – oder ganz ohne. <em>Keine Bindung, keine Kompromisse.</em>",
+        "featureTiles.fullyAccessible": "Vollständig barrierefrei",
+        "featureTiles.fullyAccessibleMessage":
+            "Entwickelt mit Barrierefreiheit, Internationalisierung (71 Sprachen direkt verfügbar) und starkem Entwicklererlebnis. <em>Jeder kann es nutzen, überall.</em>",
+        "featureTiles.flexible": "Flexibel",
+        "featureTiles.flexibleMessage":
+            "Komponenten lassen sich einfach neu gestalten, anpassen und auf dein Design abstimmen. <em>Deine Website, deine Regeln.</em>",
+
+        // Labels
+        "button.getStarted": "Los geht’s",
+        "button.ourPrivacyPromise": "Unser Datenschutzversprechen",
+
+        // Footer headings
+        "footer.heading.docs": "Docs",
+        "footer.heading.about": "Über uns",
+        "footer.heading.legal": "Rechtliches",
+
+        // Footer links (Docs)
+        "footer.content.docs.installation": "Installation",
+        "footer.content.docs.usage": "Anwendung",
+        "footer.content.docs.designTokens": "Design‐Tokens",
+        "footer.content.docs.components": "Komponenten",
+        "footer.content.docs.changelog": "Changelog",
+
+        // Footer links (About)
+        "footer.content.about.community": "Community",
+        "footer.content.about.contributing": "Mitwirken",
+        "footer.content.about.brandAssets": "Marken‐Assets",
+        "footer.content.about.support": "Support",
+
+        // Footer links (Legal)
+        "footer.content.legal.privacy": "Datenschutz",
+        "footer.content.legal.accessibility": "Barrierefreiheit",
+        "footer.content.legal.codeOfConduct": "Verhaltenskodex",
+        "footer.content.legal.licence": "Lizenz",
+        "footer.content.legal.impressum": "Impressum",
+
+        // Footer copyright
+        "footer.copyright.left": "Mit",
+        "footer.copyright.centre": "Liebe",
+        "footer.copyright.right":
+            'von Placer und ihren Mitwirkenden gemacht • <span style="white-space: nowrap">© 2025</span>',
+    },
+} as const;
