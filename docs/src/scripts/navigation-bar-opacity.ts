e3b029cba29d47c545d36ecdffa9d94a0dbc2c48
randomguy-2650 <150704902+randomguy-2650@users.noreply.github.com>
2026-01-30 22:03:32 +0100

    Optimise fonts and fix some TOC stuff

---
+++ docs/src/scripts/navigation-bar-opacity.ts
@@ -0,0 +1,25 @@
+const navigationBar = document.querySelector<HTMLElement>(".navbar");
+
+const scrollLimitPixels = 360;
+const maxOpacity = 0.9;
+
+function updateNavigationBarOpacity() {
+    const scrollY = document.body.scrollTop || window.scrollY;
+
+    let scrollProgressPercent = (scrollY / scrollLimitPixels) * 100;
+
+    scrollProgressPercent = Math.min(100, scrollProgressPercent);
+
+    const scrollRatioDecimal = scrollProgressPercent / 100;
+    const opacityDecimal = scrollRatioDecimal * maxOpacity;
+    const opacityPercent = opacityDecimal * 100;
+
+    navigationBar?.style.setProperty(
+        "--navigation-opacity",
+        opacityPercent.toFixed(1) + "%",
+    );
+}
+
+window.addEventListener("load", updateNavigationBarOpacity);
+document.body.addEventListener("scroll", updateNavigationBarOpacity);
+window.addEventListener("resize", updateNavigationBarOpacity);
